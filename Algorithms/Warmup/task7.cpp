f55718c080027630d0b382d99af82873a9b96d2a
Vladimir Petrigo <taenaru@gmail.com>
2016-03-10 20:33:45 +0300

    Solution for Time Conversion problem (Warmup)

--- Algorithms/Warmup/task7.cpp
+++ Algorithms/Warmup/task7.cpp
@@ -20,18 +20,69 @@
 
 #include <iostream>
 #include <string>
+#include <array>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
-string convert_time(const string& am_pm) {
+constexpr size_t time_parts = 3;
+constexpr size_t hours_index = 0;
+constexpr int noon_hours = 12;
 
-  return {};
+// split string into array in the format [HH, MM, SS]
+array<unsigned, time_parts> split_time(const string& am_pm) {
+  array<unsigned, time_parts> times;
+  constexpr char delim = ':';
+  size_t prev_delim_pos = 0;
+  size_t cur_delim_pos = 0;
+  size_t time_idx = 0;
+  auto input_len = am_pm.size();
+
+  while (prev_delim_pos < input_len) {
+    cur_delim_pos = am_pm.find(delim, prev_delim_pos);
+
+    if (cur_delim_pos == string::npos) {
+      cur_delim_pos = input_len;
+    }
+
+    times[time_idx++] = stoi(am_pm.substr(prev_delim_pos, cur_delim_pos - prev_delim_pos));
+    prev_delim_pos = cur_delim_pos + 1;
+  }
+
+  return times;
+}
+
+string convert_time(const string& am_pm) {
+  auto times = split_time(am_pm);
+
+  if (am_pm.rfind("PM") != string::npos && times[hours_index] != noon_hours) {
+    times[hours_index] += noon_hours;
+  }
+
+  if (am_pm.rfind("AM") != string::npos && times[hours_index] == noon_hours) {
+    times[hours_index] %= noon_hours;
+  }
+
+  ostringstream military_format;
+
+  for (size_t i = 0; i < times.size(); ++i) {
+    military_format << setw(2) << setfill('0') << times[i];
+
+    if (i + 1 != times.size()) {
+      military_format << ':';
+    }
+  }
+
+  return military_format.str();
 }
 
 int main() {
   string am_pm_time;
 
   cin >> am_pm_time;
+  auto result = convert_time(am_pm_time);
+  cout << result << endl;
 
   return 0;
 }
