bae4e3e85c4981f9f6f64eb96053e2255b5fadf4
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2022-09-03 01:57:00 +0300

    leet: algo: Speed up Pacific-Atlantic problem

--- LeetCode/Graphs/pacific_atlantic.cpp
+++ LeetCode/Graphs/pacific_atlantic.cpp
@@ -13,56 +13,33 @@
 #include <algorithm>
 #include <bitset>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
-bool internal_pa_solver(const std::vector<std::vector<int>> &heights,
-                        std::size_t i, std::size_t j,
-                        std::bitset<2> &is_pacific_atlantic,
-                        std::vector<std::vector<unsigned char>> &visited)
+void internal_pa_solver(const std::vector<std::vector<int>> &heights,
+                        std::size_t i, std::size_t j, int prev_height,
+                        const std::bitset<2> &val,
+                        std::vector<std::vector<std::bitset<2>>> &visited)
 {
-    bool is_pacific = i - 1 > i || j - 1 > j;
-    bool is_atlantic =
-        i + 1 == heights.size() || j + 1 == heights.front().size();
+    const auto height = heights.size();
+    const auto width = heights.front().size();
 
-    if (visited[i][j] != 0) {
-        return false;
+    if (i == std::numeric_limits<std::size_t>::max() ||
+        j == std::numeric_limits<std::size_t>::max() || i == height ||
+        j == width || heights[i][j] < prev_height ||
+        (visited[i][j] & val) == val) {
+        return;
     }
 
-    if (is_pacific) {
-        is_pacific_atlantic.set(0);
-    }
-
-    if (is_atlantic) {
-        is_pacific_atlantic.set(1);
-    }
-
-    if (is_pacific_atlantic.all()) {
-        return true;
-    }
-
-    visited[i][j] = 1;
-
-    // if (!is_pacific && heights[i][j] >= heights[i - 1][j]) {
-    if (i - 1 < i && heights[i][j] >= heights[i - 1][j]) {
-        internal_pa_solver(heights, i - 1, j, is_pacific_atlantic, visited);
-    }
-
-    // if (!is_atlantic && heights[i][j] >= heights[i + 1][j]) {
-    if (i + 1 < heights.size() && heights[i][j] >= heights[i + 1][j]) {
-        internal_pa_solver(heights, i + 1, j, is_pacific_atlantic, visited);
-    }
-
-    // if (!is_pacific && heights[i][j] >= heights[i][j - 1]) {
-    if (j - 1 < j && heights[i][j] >= heights[i][j - 1]) {
-        internal_pa_solver(heights, i, j - 1, is_pacific_atlantic, visited);
-    }
-
-    // if (!is_atlantic && heights[i][j] >= heights[i][j + 1]) {
-    if (j + 1 < heights.front().size() && heights[i][j] >= heights[i][j + 1]) {
-        internal_pa_solver(heights, i, j + 1, is_pacific_atlantic, visited);
-    }
-
-    return false;
+    visited[i][j] |= val;
+    internal_pa_solver(heights, i - 1, j, heights[i][j], visited[i][j],
+                       visited);
+    internal_pa_solver(heights, i + 1, j, heights[i][j], visited[i][j],
+                       visited);
+    internal_pa_solver(heights, i, j - 1, heights[i][j], visited[i][j],
+                       visited);
+    internal_pa_solver(heights, i, j + 1, heights[i][j], visited[i][j],
+                       visited);
 }
 
 std::vector<std::vector<int>>
@@ -71,21 +48,30 @@ pacific_atlantic_solver(const std::vector<std::vector<int>> &heights)
     const std::size_t height = heights.size();
     const std::size_t width = heights.front().size();
     std::vector<std::vector<int>> result;
-    std::vector<std::vector<unsigned char>> visited(
-        height, std::vector<unsigned char>(width, 0));
+    std::vector<std::vector<std::bitset<2>>> visited(
+        height, std::vector<std::bitset<2>>(width, std::bitset<2>{0}));
+
+    for (std::size_t row = 0; row < height; ++row) {
+        internal_pa_solver(heights, row, 0, std::numeric_limits<int>::min(),
+                           std::bitset<2>{}.set(0), visited);
+        internal_pa_solver(heights, row, width - 1,
+                           std::numeric_limits<int>::min(),
+                           std::bitset<2>{}.set(1), visited);
+    }
+
+    for (std::size_t col = 0; col < width; ++col) {
+        internal_pa_solver(heights, 0, col, std::numeric_limits<int>::min(),
+                           std::bitset<2>{}.set(0), visited);
+        internal_pa_solver(heights, height - 1, col,
+                           std::numeric_limits<int>::min(),
+                           std::bitset<2>{}.set(1), visited);
+    }
 
     for (std::size_t row = 0; row < height; ++row) {
         for (std::size_t col = 0; col < width; ++col) {
-            std::bitset<2> is_pacific_atlantic;
-
-            internal_pa_solver(heights, row, col, is_pacific_atlantic, visited);
-            std::for_each(std::begin(visited), std::end(visited), [](auto &el) {
-                std::fill(std::begin(el), std::end(el), 0);
-            });
-
-            if (is_pacific_atlantic.all()) {
-                result.emplace_back(std::vector<int>{static_cast<int>(row),
-                                                     static_cast<int>(col)});
+            if (visited[row][col].all()) {
+                result.push_back(
+                    {static_cast<int>(row), static_cast<int>(col)});
             }
         }
     }
@@ -142,7 +128,7 @@ TEST_CASE("Pacific-Atlantic basic test")
 
     CHECK(std::size(result) == std::size(expected));
     CHECK(std::equal(std::cbegin(result), std::cend(result),
-                     std::cbegin(result)));
+                     std::cbegin(expected)));
 }
 
 TEST_CASE("Pacific-Atlantic single cell")
@@ -153,7 +139,36 @@ TEST_CASE("Pacific-Atlantic single cell")
 
     CHECK(std::size(result) == std::size(expected));
     CHECK(std::equal(std::cbegin(result), std::cend(result),
-                     std::cbegin(result)));
+                     std::cbegin(expected)));
+}
+
+TEST_CASE("Pacific-Atlantic mountains")
+{
+    const std::vector<std::vector<int>> heights{
+        {3, 3, 3, 3, 3, 3}, {3, 0, 3, 3, 0, 3}, {3, 3, 3, 3, 3, 3}};
+    const auto result = pacific_atlantic_solver(heights);
+    auto expected{std::vector<std::vector<int>>{
+        {0, 0},
+        {0, 1},
+        {0, 2},
+        {0, 3},
+        {0, 4},
+        {0, 5},
+        {1, 0},
+        {1, 2},
+        {1, 3},
+        {1, 5},
+        {2, 0},
+        {2, 1},
+        {2, 2},
+        {2, 3},
+        {2, 4},
+        {2, 5},
+    }};
+
+    CHECK(std::size(result) == std::size(expected));
+    CHECK(std::equal(std::cbegin(result), std::cend(result),
+                     std::cbegin(expected)));
 }
 
 TEST_CASE("Pacific-Atlantic max size")
@@ -165,14 +180,14 @@ TEST_CASE("Pacific-Atlantic max size")
 
     for (std::size_t i = 0; i < 200; ++i) {
         for (std::size_t j = 0; j < 200; ++j) {
-            expected[i + j] =
+            expected[i * heights.front().size() + j] =
                 std::vector<int>{static_cast<int>(i), static_cast<int>(j)};
         }
     }
 
     CHECK(std::size(result) == std::size(expected));
     CHECK(std::equal(std::cbegin(result), std::cend(result),
-                     std::cbegin(result)));
+                     std::cbegin(expected)));
 }
 
 TEST_CASE("Pacific-Atlantic spiral case")
@@ -188,5 +203,5 @@ TEST_CASE("Pacific-Atlantic spiral case")
 
     CHECK(std::size(result) == std::size(expected));
     CHECK(std::equal(std::cbegin(result), std::cend(result),
-                     std::cbegin(result)));
+                     std::cbegin(expected)));
 }
